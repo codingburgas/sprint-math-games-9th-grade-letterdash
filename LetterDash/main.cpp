63a2a6a02e07a065d188cc38cdfc8076e82961e5
JYStoykova24 <JYStoykova24@codingburgas.bg>
2025-11-23 18:22:31 +0200

    start project

--- LetterDash/main.cpp
+++ LetterDash/main.cpp
@@ -6,37 +6,38 @@ using namespace std;
 
 // "Enter a word" function
 void wordInput(string& word) {
-    string bannedSymbols = "0123456789!@#$%^&*()_+-=<>?/\\|[]{},.;:'\" "; 
+    string bannedSymbols = "0123456789!@#$%^&*()_+-=<>?/\\|[]{},.;:'\" ";
 
     cout << "The word must not contain numeric characters, symbols, or spaces\n";
     cout << "Enter a word: ";
     while (true) {
         try {
-            cin.ignore(numeric_limits<streamsize>::max(),'\n');
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
             cout << "> ";
 
             getline(cin, word);
-            
 
-            if(word.empty()) throw runtime_error("The word must not be empty");
-            
+
+            if (word.empty()) throw runtime_error("The word must not be empty");
+
             for (size_t i = 0; i < word.size(); i++) {
                 if (bannedSymbols.find(word[i]) != string::npos) {
                     throw std::runtime_error("The word must not contain numeric characters, symbols, or spaces");
                 }
 
             }
-            break;        
-        } catch (const exception& e) {
+            break;
+        }
+        catch (const exception& e) {
             cout << e.what() << "\n";
         }
     }
 }
 
 
-int main(){
+int main() {
     string word;
-    
+
     cout << R"(
              _____________
             |/ \    |    /
@@ -57,18 +58,18 @@ int main(){
             You pick: )";
 
     int option;
-    while (true) {   
+    while (true) {
 
-    cin >> option;
+        cin >> option;
 
-    switch (option) {
+        switch (option) {
         case 1:
             wordInput(word);
             system("clear");
             break;
 
         case 2:
-            
+
             word = "";
             break;
 
@@ -78,8 +79,8 @@ int main(){
         default:
             cout << "Invalid option\n";
             break;
-        
-    }
+
+        }
     }
     return 0;
 }
